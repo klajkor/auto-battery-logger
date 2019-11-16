5cd95afe5f17502c78239d0b4421a9639abc0f21
Robert Klajko <klajkor@gmail.com>
2019-11-16 19:29:57 +0100

    loop without sleep is ready

--- src/main.cpp
+++ src/main.cpp
@@ -1,191 +1,306 @@
-#include <Arduino.h>
+/**
+* Arduino automotive battery voltage logger
+*
+* @author Robert Klajko
+* @url https://github.com/klajkor/auto-battery-logger.git
+*
+* Board: Arduino Pro Mini (or Nano)
+*
+* Extension modules and sensors used:
+*  - INA219, I2C
+*  - DS3231 RTC, I2C
+*  - SD card, SPI
+*  - DHT22 (AM2302) temperature and humidity sensor
+* Libraries used:
+*  - Adafruit INA219 by Adafruit - Copyright (c) 2012, Adafruit Industries
+*  - SD by Adafruit Industries - Copyright (c) 2012, Adafruit Industries
+*  - uRTCLib by Naguissa - Copyright (c) 2019, Naguissa
+*
+* BSD license, all text here must be included in any redistribution.
+*
+*/
+
+/** 
+* Arduino  pinout connections
+*
+* I2C bus:
+* - SCK - pin A5
+* - SDA - pin A4
+*
+* SD card on SPI bus:
+* - MOSI - pin D11
+* - MISO - pin D12
+* - CLK - pin D13
+* - CS - pin D10
+*
+* DHT22 sensor:
+* - DATA - pin D4
+*/
 
 /**
- * Author:Ab Kurk
- * version: 1.0
- * date: 4/02/2018
- * Description: 
- * This sketch is part of the guide to putting your Arduino to sleep
- * tutorial. We use the:
- * Adafruit DS3231 RTC
- * Adafruit 5V ready Micro SD break out board
- * Arduino Pro Mini
- * DHT11 or DHT22 humidety/Temperature sensor
- * In this example we use the RTC to wake up the Arduino to log the temp and humidity on to an SD card.
- * After the data has been logged the Arduino goes back to sleep and gets woken up 5 minutes later to 
- * start all over again
- * Link To Tutorial http://www.thearduinomakerman.info/blog/2018/1/24/guide-to-arduino-sleep-mode
- * Link To Project   http://www.thearduinomakerman.info/blog/2018/2/5/wakeup-rtc-datalogger
+ * Toolchain: VSCode + Platform.IO 
  */
 
-#include <avr/sleep.h> //this AVR library contains the methods that controls the sleep modes
-#define interruptPin 2 //Pin we are going to use to wake up the Arduino
-#include <DS3232RTC.h> //RTC Library https://github.com/JChristensen/DS3232RTC
-#include "DHT.h"       // Adafruit DHT Library for the DHT 11 or DHT 22 https://github.com/adafruit/DHT-sensor-library/archive/master.zip
-#include <SPI.h>       //SPI library for the sd breakout board. Part of the Arduino IDE
-#include <SD.h>        //SD library for the sd breakout board. Part of the Arduino IDE
+#include <Arduino.h>
+#include <math.h>
+#include <uRTCLib.h>
+#include <SD.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+#include <avr/sleep.h> //AVR libraryfor sleep modes
+#include "DHT.h"
 
-//Variables needed for the DHT (Humidity/Temperature Sensor
-#define DHTPIN 4 // what digital pin we're connected  the DHT sensor to
+/* Declarations and initializations */
 
-// Uncomment whatever type you're using!
-#define DHTTYPE DHT11 // DHT 11
-//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-DHT dht(DHTPIN, DHTTYPE); // Creating the DHT object to read from the DHT Sensor
-//card reader variabls
-File myFile;               //Creating the file object so we can write to the SD card
-const int chipSelect = 10; // The digital pin needed for the Adafruit SD card breakout board
-//RTC Module global variables
-const int time_interval = 5; // Sets the wakeup intervall in minutes
 
-void Going_To_Sleep();
-void wakeUp();
-void temp_Humi();
-void writeData(float h, float t, float f);
+// Current and voltage sensor class
+Adafruit_INA219 ina219_monitor;
 
+// DS3231 RTC modul I2C address
+const int RTC_I2C_addr = 0x68;
+// RTC class
+uRTCLib rtc(RTC_I2C_addr);
+
+// SD card modul chip select
+#define SDCARD_CHIP_SELECT 10
+
+//DHT22 Humidity/Temperature Sensor
+#define DHTPIN 4 // DHT sensor pin
+#define DHTTYPE DHT22 // DHT DHT 22  (AM2302)
+DHT dht(DHTPIN, DHTTYPE);
+
+#define WakeUpInterruptPin 2 //INT0 is on pin D2 in Arduino
+
+/* Global variables */
+
+/* RTC global variables */
+uint8_t rtc_second = 0;
+uint8_t rtc_minute = 0;
+uint8_t rtc_hour = 0;
+uint8_t rtc_day = 0;
+uint8_t rtc_month = 0;
+uint8_t rtc_year = 0;
+
+// Current sensor variables
+float f_BusVoltage_V; /** Measured bus voltage */
+float f_ShuntCurrent_mA; /** Measured shunt current */
+
+
+// General variables
+char DateStampString[] = "2000.99.88"; /** String to store date value */
+char TimeStampString[] = "00:00:00"; /** String to store time value */
+char logFileName[] = "mmddHHMM.txt"; /** String to store log file name */
+char VoltString[] = "99.999 "; /** String to store measured voltage value */
+char CurrentString[] = "9999.999 "; /** String to store measured current value */
+bool SD_log_enabled = false; /** Enabling SD logging or not */
+
+// Datafile class
+File dataFile;
+
+//DHT22 measured values
+float temperature=0; /** in Celsius degree */
+float humidity=0;
+char TemperatureString[] = "999.9 ";
+char HumidityString[] = "999.99 ";
+
+/* Function definitions */
+
+bool Log_To_SD_card(void);
+void setTimeStampString(void);
+void ina219_Init(void);
+void SD_Card_Init(void);
+void GPIO_Init(void);
+void dht22_Init(void);
+void get_Temp_Humid(void);
+void get_Voltage_Current(void);
+
+//setup()
 void setup()
 {
-  Serial.begin(115200);                //Start Serial Comunication
-  pinMode(LED_BUILTIN, OUTPUT);        //We use the led on pin 13 to indecate when Arduino is A sleep
-  pinMode(interruptPin, INPUT_PULLUP); //Set pin d2 to input using the buildin pullup resistor
-  digitalWrite(LED_BUILTIN, HIGH);     //turning LED on
-
-  // initialize the alarms to known values, clear the alarm flags, clear the alarm interrupt flags
-  RTC.setAlarm(ALM1_MATCH_DATE, 0, 0, 0, 1);
-  RTC.setAlarm(ALM2_MATCH_DATE, 0, 0, 0, 1);
-  RTC.alarm(ALARM_1);
-  RTC.alarm(ALARM_2);
-  RTC.alarmInterrupt(ALARM_1, false);
-  RTC.alarmInterrupt(ALARM_2, false);
-  RTC.squareWave(SQWAVE_NONE);
-  time_t t;                                                             //create a temporary time variable so we can set the time and read the time from the RTC
-  t = RTC.get();                                                        //Gets the current time of the RTC
-  RTC.setAlarm(ALM1_MATCH_MINUTES, 0, minute(t) + time_interval, 0, 0); // Setting alarm 1 to go off 5 minutes from now
-  // clear the alarm flag
-  RTC.alarm(ALARM_1);
-  // configure the INT/SQW pin for "interrupt" operation (disable square wave output)
-  RTC.squareWave(SQWAVE_NONE);
-  // enable interrupt output for Alarm 1
-  RTC.alarmInterrupt(ALARM_1, true);
-
-  dht.begin(); //Start the DHT sensor
-  /**Initializes the SD breakout board. If it is not ready or not connected correct it writes
-     * an error message to the serial monitor
-     **/
-  Serial.print("Initializing SD card...");
-  if (!SD.begin(chipSelect))
-  {
-    Serial.println("initialization failed!");
-    return;
-  }
-  Serial.println("initialization done.");
-}
+  Serial.begin(115200);
+  GPIO_Init();
+  Wire.begin();
+  delay(100);
+  ina219_Init();
+  SD_Card_Init();
+  dht22_Init();
+  setTimeStampString();
+    
+} 
 
 void loop()
 {
-  delay(5000); //wait 5 seconds before going to sleep. In real senairio keep this as small as posible
-  Going_To_Sleep();
-}
-
-void Going_To_Sleep()
-{
-  sleep_enable();                                                                                        //Enabling sleep mode
-  attachInterrupt(0, wakeUp, LOW);                                                                       //attaching a interrupt to pin d2
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN);                                                                   //Setting the sleep mode, in our case full sleep
-  digitalWrite(LED_BUILTIN, LOW);                                                                        //turning LED off
-  time_t t;                                                                                              // creates temp time variable
-  t = RTC.get();                                                                                         //gets current time from rtc
-  Serial.println("Sleep  Time: " + String(hour(t)) + ":" + String(minute(t)) + ":" + String(second(t))); //prints time stamp on serial monitor
-  delay(1000);                                                                                           //wait a second to allow the led to be turned off before going to sleep
-  sleep_cpu();                                                                                           //activating sleep mode
-  Serial.println("just woke up!");                                                                       //next line of code executed after the interrupt
-  digitalWrite(LED_BUILTIN, HIGH);                                                                       //turning LED on
-  temp_Humi();                                                                                           //function that reads the temp and the humidity
-  t = RTC.get();
-  Serial.println("WakeUp Time: " + String(hour(t)) + ":" + String(minute(t)) + ":" + String(second(t))); //Prints time stamp
-  //Set New Alarm
-  RTC.setAlarm(ALM1_MATCH_MINUTES, 0, minute(t) + time_interval, 0, 0);
-
-  // clear the alarm flag
-  RTC.alarm(ALARM_1);
-}
-
-void wakeUp()
-{
-  Serial.println(F("IF")); //Print message to serial monitor
-  sleep_disable();         //Disable sleep mode
-  detachInterrupt(0);      //Removes the interrupt from pin 2;
-}
-
-//This function reads the temperature and humidity from the DHT sensor
-void temp_Humi()
-{
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity(); //reads humidity
-  // Read temperature as Celsius (the default)
-  float t = dht.readTemperature();
-  // Read temperature as Fahrenheit (isFahrenheit = true)
-  float f = dht.readTemperature(true);
-  writeData(h, t, f); //sends the data to the writeData function
-  Serial.print("Humidity: ");
-  Serial.print(h);
-  Serial.print(" %\t");
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.print(" *F\t");
-}
-/**
-  * the writeData function gets the humidity (h), temperature in celsius (t) and farenheit (f) as input 
-  * parameters. It uses the RTC to create a filename using the current date, and writes the temperature
-  * and humidity with a date stamp to this file
-  */
-void writeData(float h, float t, float f)
-{
-  time_t p;                                                                               //create time object for time and date stamp
-  p = RTC.get();                                                                          //gets the time from RTC
-  //String file_Name = String(day(p)) + monthShortStr(month(p)) + String(year(p)) + ".txt"; //creates the file name we are writing to.
-  char file_Name[]="log.txt";
-  myFile = SD.open(file_Name, FILE_WRITE);                                                // creates the file object for writing
-
-  // if the file opened okay, write to it:
-  if (myFile)
-  {
-    Serial.print(F("Writing to "));
-    Serial.print(file_Name);
-    //appends a line to the file with time stamp and humidity and temperature data
-    myFile.println(String(hour(p)) + ":" + String(minute(p)) + " Hum: " + String(h) + "% C: " + String(t) + " F: " + String(f));
-    // close the file:
-    myFile.close();
-    Serial.println(F("done."));
-  }
-  else
-  {
-    // if the file didn't open, print an error:
-    Serial.println(F("error opening "));
-    Serial.print(file_Name);
-  }
-  //opening file for reading and writing content to serial monitor
-  myFile = SD.open(file_Name);
-  if (myFile)
-  {
-    Serial.println(file_Name);
-
-    // read from the file until there's nothing else in it:
-    while (myFile.available())
-    {
-      Serial.write(myFile.read());
+  setTimeStampString();
+  
+  //display time stamp
+  Serial.print(DateStampString);
+  Serial.print(F(" "));
+  Serial.print(TimeStampString);
+  Serial.print(F(" "));
+  get_Voltage_Current();
+  get_Temp_Humid();
+  // log all data to file
+  if(SD_log_enabled) {
+    Serial.print(F("SD log: "));
+    Serial.print(logFileName);
+    if (Log_To_SD_card()) {
+      Serial.println(F(" OK"));      
     }
-    // close the file:
-    myFile.close();
+    else {
+      Serial.println(F(" failed!"));      
+    }
+    
   }
-  else
-  {
-    // if the file didn't open, print an error:
-    Serial.println(F("error opening"));
-    Serial.print(file_Name);
+  delay(4500);
+
+}
+
+/**
+* @brief Query date & time and set the [Date|Time]StampStrings & logFileName accordingly.
+* @param void
+*/
+void setTimeStampString(void)
+{
+  // get time stamp, convert to a string
+  rtc.refresh();
+  rtc_second = rtc.second();
+  rtc_minute = rtc.minute();
+  rtc_hour = rtc.hour();
+  rtc_day = rtc.day();
+  rtc_month = rtc.month();
+  rtc_year = rtc.year();
+
+  DateStampString[2] = (char) ((rtc_year / 10)+0x30);
+  DateStampString[3] = (char) ((rtc_year % 10)+0x30);
+  DateStampString[5] = (char) ((rtc_month / 10)+0x30);
+  DateStampString[6] = (char) ((rtc_month % 10)+0x30);
+  DateStampString[8] = (char) ((rtc_day / 10)+0x30);
+  DateStampString[9] = (char) ((rtc_day % 10)+0x30);
+  
+  TimeStampString[0] = (char) ((rtc_hour / 10)+0x30);
+  TimeStampString[1] = (char) ((rtc_hour % 10)+0x30);
+  TimeStampString[3] = (char) ((rtc_minute / 10)+0x30);
+  TimeStampString[4] = (char) ((rtc_minute % 10)+0x30);
+  TimeStampString[6] = (char) ((rtc_second / 10)+0x30);
+  TimeStampString[7] = (char) ((rtc_second % 10)+0x30);
+
+  logFileName[0] = DateStampString[0];
+  logFileName[1] = DateStampString[1];
+  logFileName[2] = DateStampString[2];
+  logFileName[3] = DateStampString[3];
+  logFileName[4] = DateStampString[5];
+  logFileName[5] = DateStampString[6];
+  logFileName[6] = DateStampString[8];
+  logFileName[7] = DateStampString[9];
+    
+}
+
+/**
+* @brief Log the measurements with timestamp to SD card in CSV format.
+* @param void
+* @return bool FileOpenSuccess
+*/
+bool Log_To_SD_card(void)
+{
+  bool FileOpenSuccess = false;
+  
+  dataFile = SD.open(logFileName, FILE_WRITE);
+  //dataFile = SD.open(_logfile, FILE_WRITE);
+  
+  // if the file is available, write to it:
+  if (dataFile) {
+    FileOpenSuccess=true;
   }
+  else {
+    FileOpenSuccess=false;
+    Serial.println(F(" logfile open error!"));
+  }
+  
+  if (FileOpenSuccess) {
+    dataFile.print(DateStampString);
+    dataFile.print(F(","));
+    dataFile.print(TimeStampString);
+    dataFile.print(F(","));
+    dataFile.print(VoltString);
+    dataFile.print(F(",V,"));
+    dataFile.print(CurrentString);
+    dataFile.print(F(",mA,"));
+    dataFile.print(TemperatureString);
+    dataFile.print(F(",C,"));
+    dataFile.print(HumidityString);
+    dataFile.println(F(",%"));
+    dataFile.close();    
+  }
+  
+  return FileOpenSuccess;
+}
+
+void ina219_Init(void)
+{
+  ina219_monitor.begin();
+  //Serial.println(F("INA219 begin done"));
+  // begin calls:
+  // configure() with default values RANGE_32V, GAIN_8_320MV, ADC_12BIT, ADC_12BIT, CONT_SH_BUS
+  // calibrate() with default values D_SHUNT=0.1, D_V_BUS_MAX=32, D_V_SHUNT_MAX=0.2, D_I_MAX_EXPECTED=2
+  // in order to work directly with ADAFruit's INA219B breakout  
+}
+
+void SD_Card_Init(void)
+{
+  Serial.print(F("SD card "));
+  
+  // see if the card is present and can be initialized:
+  if (!SD.begin(SDCARD_CHIP_SELECT)) {
+    Serial.println(F(" failed"));
+    SD_log_enabled = false;
+  }
+  else {
+    SD_log_enabled = true;
+    Serial.println(F(" OK"));    
+  }
+  delay(200);
+  
+}
+
+void GPIO_Init(void)
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(WakeUpInterruptPin, INPUT_PULLUP);
+  digitalWrite(LED_BUILTIN, HIGH);     //turning LED on
+}
+
+void dht22_Init(void)
+{
+  dht.begin(); //Starts the DHT sensor
+}
+
+void get_Temp_Humid(void)
+{
+  temperature=dht.readTemperature(); // Read temperature as Celsius (default setting)
+  humidity=dht.readHumidity();
+  dtostrf((temperature),5,1,TemperatureString);
+  dtostrf(humidity,6,2,HumidityString);
+  Serial.print(F("Temp:"));
+  Serial.print(TemperatureString);
+  Serial.print(F(" *C "));
+  Serial.print(F("Humid:"));
+  Serial.print(HumidityString);
+  Serial.println(F(" %"));
+}
+
+void get_Voltage_Current(void)
+{
+  //measure voltage and current
+  f_ShuntCurrent_mA=ina219_monitor.getCurrent_mA();
+  f_BusVoltage_V=ina219_monitor.getBusVoltage_V();
+  //convert to text
+  dtostrf((f_ShuntCurrent_mA),7,2,CurrentString);
+  dtostrf(f_BusVoltage_V,6,3,VoltString);
+  //display volt
+  Serial.print(VoltString);
+  Serial.print(F(" V"));
+  //display current
+  Serial.print(CurrentString);
+  Serial.println(F(" mA "));
 }
